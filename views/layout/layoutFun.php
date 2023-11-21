028efcc4e137e33d44f67849dcdfcf09f328fa10
tiagonilvany@gmail.com <you@example.com>
2023-11-21 19:51:32 +0100

    main

--- views/layout/layoutFun.php
+++ views/layout/layoutFun.php
@@ -156,7 +156,7 @@ require_once('views\layout\session.php');
                             <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
                             <path d="M16 3.13a4 4 0 0 1 0 7.75" />
                         </svg>
-                        Lists
+                        Groups
 
                         <svg class="hs-accordion-active:block ms-auto hidden w-4 h-4 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
