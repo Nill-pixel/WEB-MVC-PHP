579db64e17c0afcfbfbba9c4354ccb2b8de9ece6
Nill-pixel <tiagonilvany@gmail.com>
2024-01-18 20:52:16 +0100

    Swall Error

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -85,7 +85,6 @@ class TaskController extends RenderViews
 
     public function showImportant()
     {
-        echo '<script>swal("Good job!", "You clicked the button!", "success");</script>';
         $this->loadView('tasks', [
             'tasks' => $this->task->allImportant(),
             'lists' => $this->list->getList()
@@ -114,7 +113,6 @@ class TaskController extends RenderViews
 
     public function taskCompleted()
     {
-        echo '<script>swal("Good job!", "You clicked the button!", "success");</script>';
         $this->loadView('tasks', [
             'tasks' => $this->task->taskCompleted(),
             'lists' => $this->list->getList()
